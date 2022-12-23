34403944bbec788e5e24d84b75bf05a720eaead3
Angelina <kuzmenkoa022@gmail.com>
2022-12-23 15:18:41 +0200

    tab

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as openSettFunc from './modules/openSett.js'
 import * as settingsFunc from './modules/settings.js';
 import * as progressFunc from './modules/progress.js';
+import * as tabFunc from './modules/tabs.js'
 
 openSettFunc.openSett();
 settingsFunc.settings();
 progressFunc.progressBar();
+tabFunc.tab();
